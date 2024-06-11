e98ff432ef1c590af91a92f2d077970a810dbf48
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-06-11 14:58:47 +0200

    created styled file

---
+++ src/features/PersonalHomepage/Footer/SocialIcon/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledFooterSocials = styled.ul`
+  padding: 0;
+  display: flex;
+  gap: 24px;
+  margin-top: 56px;
+  margin-bottom: 0;
+`;
+
+export const StyledSocialItem = styled.li`
+  list-style: none;
+`;

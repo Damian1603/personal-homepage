e53b5c0cac15d7af9303986c535c4ae25eca97b1
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-07-29 16:36:35 +0200

    moved unnecessary

--- src/saga.js
+++
@@ -1,6 +0,0 @@
-import { all, call } from "redux-saga/effects";
-import { PersonalHomepage } from "./features/PersonalHomepage";
-
-export default function* saga() {
-  yield all([call(PersonalHomepage)])
-};
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import themeReducer from "./features/PersonalHomepage/Header/ThemeButton/themeSlice";
-
-export const store = configureStore({
-    reducer: {
-        theme: themeReducer,
-    },
-});

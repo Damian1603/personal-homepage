bcebb7bf618e3fe31d45fedb0416b85f6b2f04a9
Damian Kuczyński <kuczynski.damian00@wp.pl>
2025-02-04 12:17:01 +0100

    added email as a component

--- src/features/PersonalHomepage/Footer/index.js
+++ src/features/PersonalHomepage/Footer/index.js
@@ -4,10 +4,11 @@ import {
   StyledFooterDescription,
 } from "./styled";
 import { StyledCaption } from "../Header/Description/styled";
-import { SocialIcon } from "./SocialIcon";
+import { SocialIcons } from "./SocialIcons";
 import { StyledLink } from "./styled";
+import { email } from "../email";
 
-export const Footer = ({ email }) => {
+export const Footer = () => {
 
   return (
     <StyledFooterWrapper>
@@ -16,13 +17,13 @@ export const Footer = ({ email }) => {
       </StyledCaption>
       <StyledFooterEmail>
         <StyledLink href={`mailto:${email}`}>
-          kuczynski.damian1603@gmail.com
+          {email}
         </StyledLink>
       </StyledFooterEmail>
       <StyledFooterDescription>
         I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to contact me
       </StyledFooterDescription>
-      <SocialIcon />
+      <SocialIcons />
     </StyledFooterWrapper>
   )
 };

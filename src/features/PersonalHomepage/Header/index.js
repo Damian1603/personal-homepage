4344ff47a7ce35e3be32e96b27e8a731454eb022
Damian Kuczyński <kuczynski.damian00@wp.pl>
2025-02-11 09:30:57 +0100

    created Header component

---
+++ src/features/PersonalHomepage/Header/index.js
@@ -0,0 +1,10 @@
+import { StyledWrapper } from "./styled";
+import { Image } from "./Image";
+import { Description } from "./Description";
+
+export const Header = () => (
+  <StyledWrapper>
+    <Image />
+    <Description />
+  </StyledWrapper>
+);

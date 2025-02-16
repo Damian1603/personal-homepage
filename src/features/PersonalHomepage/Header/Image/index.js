9994458de94616118242fcbcefb3d95efa2bbc1e
Damian Kuczyński <kuczynski.damian00@wp.pl>
2025-02-16 13:49:43 +0100

    created Image component and imported styled component which i named StyleImage into it

---
+++ src/features/PersonalHomepage/Header/Image/index.js
@@ -0,0 +1,6 @@
+import { StyledImage } from "./styled";
+import portrait from "../../../../images/damian.jpg";
+
+export const Image = () => (
+    <StyledImage src={portrait} alt="Damian Kuczynski"></StyledImage>
+);
---
+++ src/features/PersonalHomepage/Header/Image/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledImage = styled.img`
+  border-radius: 50%;
+  width: 30vw;
+  max-width: 380px;
+  max-height: 394px;
+`;
